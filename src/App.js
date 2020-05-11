a136f41d9535cbc73defa01cc52d48e1e810239e
Dimitry Wuyts <dimiw1337@hotmail.com>
2020-05-11 11:00:32 +0200

    tried to make this api work with imane but there is something wrong ( nothing wrong with api and synyax ) but for some reason its not working

--- src/App.js
+++ src/App.js
@@ -1,44 +1,53 @@
-import React, {Component} from 'react';
+import React, {useEffect, useState} from 'react';
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      data: [],
-      isLoaded: false,
-    }
-  }
+function App (){
 
-  componentDidMount() {
-    fetch('https://prime.exchangerate-api.com/v5/4f2c91e9553da2e933e007ea/latest/USD')
-        .then(response => response.json())
-        .then(data => {
-          this.setState({
-            isLoaded: true,
-            USD: data,
-          })
-        });
-  }
+    const [isLoaded, setIsLoaded]=useState(false);
+    const [data, setData]=useState([]);
 
-  render() {
-    console.log()
 
-    var {isLoaded, data} = this.state;
+    useEffect(() =>{
+    async function fetchData() {
+        const response = await fetch ("https://api.exchangeratesapi.io/latest");
+        setIsLoaded (true);
+        const fetchData = await response.json();
+        }
+        setData(fetchData);
+        },[]);
+        console.log (data)
+
+  // componentDidMount() {
+  //   fetch('https://prime.exchangerate-api.com/v5/4f2c91e9553da2e933e007ea/latest/USD')
+  //       .then(response => response.json())
+  //       .then(data => {
+  //         this.setState({
+  //           isLoaded: true,
+  //           USD: data,
+  //         }) .catch
+  //       });
+  // }
+
+
     if (!isLoaded) {
       return <div>loading...</div>;
     } else {
       return (
           <div className="App">
             <ul>
-              {data.map(data => (
+              { data.rates === undefined
+              ?
+                  "fetch did not work"
+                :
+                  data.map(data => (
                 <li key={data.id}>
-               result: {data.result} | documentation {data.documentation}
+               result: {data.rates}
                 </li>
+
               ))}
             </ul>
           </div>
       );
     }
-  }
+
 }
 export default App
