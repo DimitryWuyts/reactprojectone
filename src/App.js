3b976d5131255f28da4e4d68e5c1cbe4d41a54e0
Dimitry Wuyts <dimiw1337@hotmail.com>
2020-05-11 14:53:29 +0200

    updated src

--- src/App.js
+++ src/App.js
@@ -1,53 +1,79 @@
-import React, {useEffect, useState} from 'react';
+// import React, {useEffect, useState} from 'react';
+//
+// function App (){
+//
+//     const [isLoaded, setIsLoaded]=useState(false);
+//     const [data, setData]=useState([]);
+//
+//
+//     useEffect(() =>{
+//     async function fetchData() {
+//         const response = await fetch ("https://api.exchangeratesapi.io/latest");
+//         const jsonData = await response.json();
+//         setData(jsonData);
+//         console.log (data)
+//         }
+//         fetchData()
+//         },[]);
+//
+//
+//   // componentDidMount() {
+//   //   fetch('https://prime.exchangerate-api.com/v5/4f2c91e9553da2e933e007ea/latest/USD')
+//   //       .then(response => response.json())
+//   //       .then(data => {
+//   //         this.setState({
+//   //           isLoaded: true,
+//   //           USD: data,
+//   //         }) .catch
+//   //       });
+//   // }
+//
+//
+//     if (!isLoaded) {
+//       return <div>loading...</div>;
+//     } else {
+//       return (
+//           <div className="App">
+//             <ul>
+//               { data === undefined
+//               ?
+//                   "fetch did not work"
+//                 :
+//                   data.map(data => (
+//                 <li key={data.id}>
+//                result: {data.rates}
+//                 </li>
+//
+//               ))}
+//             </ul>
+//           </div>
+//       );
+//     }
+//
+// }
+// export default App
 
-function App (){
 
-    const [isLoaded, setIsLoaded]=useState(false);
-    const [data, setData]=useState([]);
+import React, { useState, useEffect } from "react";
 
-
-    useEffect(() =>{
+function App  (){
+    const [data, setData] = useState([]);
+    const [loading, setLoading] = useState(true);
     async function fetchData() {
-        const response = await fetch ("https://api.exchangeratesapi.io/latest");
-        setIsLoaded (true);
-        const fetchData = await response.json();
-        }
-        setData(fetchData);
-        },[]);
-        console.log (data)
-
-  // componentDidMount() {
-  //   fetch('https://prime.exchangerate-api.com/v5/4f2c91e9553da2e933e007ea/latest/USD')
-  //       .then(response => response.json())
-  //       .then(data => {
-  //         this.setState({
-  //           isLoaded: true,
-  //           USD: data,
-  //         }) .catch
-  //       });
-  // }
-
-
-    if (!isLoaded) {
-      return <div>loading...</div>;
-    } else {
-      return (
-          <div className="App">
-            <ul>
-              { data.rates === undefined
-              ?
-                  "fetch did not work"
-                :
-                  data.map(data => (
-                <li key={data.id}>
-               result: {data.rates}
-                </li>
-
-              ))}
-            </ul>
-          </div>
-      );
+        const response = await fetch('https://api.exchangeratesapi.io/latest');
+        const json = await response.json();
+        setData(json);
+        setLoading(false)
     }
 
-}
-export default App
+    useEffect(() => {
+        fetchData()
+
+    }, []);
+
+    return (<div>
+        <h1>{data.map}{loading}</h1>
+    </div>);
+};
+
+export default App;
